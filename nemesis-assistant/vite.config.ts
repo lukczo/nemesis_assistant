61874f0a3128ec6bc1042e3d6f4ff7ae1ca5a608
ukwial <lukczo@outlook.com>
2023-05-13 23:27:19 +0200

    WPA added
    wip

--- nemesis-assistant/vite.config.ts
+++ nemesis-assistant/vite.config.ts
@@ -1,8 +1,38 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import eslint from 'vite-plugin-eslint';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: "Nemesis Assistant",
+        short_name: "NA",
+        icons: [
+          {
+            src: "alien.ico",
+            sizes: "64x64",
+            type: "image/x-icon",
+          },
+          {
+            src: "alien128.png",
+            type: "image/png",
+            sizes: "128x128",
+          },
+          {
+            src: "alien512.png",
+            type: "image/png",
+            sizes: "512x512",
+          },
+        ],
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        scope: "/",
+        orientation: "portrait",
+      },
+    }),
+  ],
+});
