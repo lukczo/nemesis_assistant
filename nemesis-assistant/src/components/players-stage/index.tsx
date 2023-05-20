7090a1cc932feb7cd334d29bad26c7aa09ad7035
ukwial <lukczo@outlook.com>
2023-05-20 14:03:24 +0200

    adding carousel
    wip

--- nemesis-assistant/src/components/players-stage/index.tsx
+++ nemesis-assistant/src/components/players-stage/index.tsx
@@ -1,21 +1,71 @@
 import { useDrawFromBag } from "../../helpers/bag";
 import { NightStalker } from "../../shared-types";
-import { useState } from "react";
+import { ComponentProps, ReactNode, useState } from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
+import cls from "./index.module.css";
+import { useGlobalState } from "../../global-state";
 
 // interface PlayersStageProps {}
 
 export const PlayersStage = (/* p: PlayersStageProps */) => {
   const [drawnStalker, setDrawnStalker] = useState<NightStalker | null>(null);
+  const [players] = useGlobalState("players");
+  const [bag] = useGlobalState("bag");
 
   const drawFromBag = useDrawFromBag(true);
 
   return (
-    <div>
-      {drawnStalker === null ? "null" : drawnStalker}
-      <br />
-      <button onClick={() => setDrawnStalker(drawFromBag())}>
-        LOSUJ POTWORA
-      </button>
+    <div style={{ overflowY: "hidden" }}>
+      <Carousel
+        centerMode
+        infiniteLoop
+        showStatus={false}
+        showIndicators={false}
+      >
+        <Tile header={"Main"}>
+          {players.map((x) => {
+            if (!x) return "";
+            return (
+              <div>
+                {x?.name + " " + x?.character} <br />
+              </div>
+            );
+          })}
+        </Tile>
+        <Tile header={"Woreczek"}>
+          {drawnStalker === null ? "null" : drawnStalker}
+          <br />
+          <button onClick={() => setDrawnStalker(drawFromBag())}>
+            LOSUJ POTWORA
+          </button>
+          <br />
+          Monsters: {bag.length} <br />{" "}
+          {bag.map((stalker) => (
+            <div>
+              {stalker}
+              {stalker === null && "empty token"}
+              <br />
+            </div>
+          ))}
+        </Tile>
+        <Tile header={"Laboratorium"}>lalal</Tile>
+      </Carousel>
+    </div>
+  );
+};
+
+type TileProps = {
+  header?: ReactNode;
+} & ComponentProps<"div">;
+
+export const Tile = (p: TileProps) => {
+  return (
+    <div {...p} className={cls.tile}>
+      <div>
+        <h2>{p.header}</h2>
+      </div>
+      {p.children}
     </div>
   );
 };
